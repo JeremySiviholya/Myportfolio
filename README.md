3015b7f948d1fc8d30d5c1fe19ccd73154e157a5
jeremy <jeremysiviholya@gmail.com>
2022-12-02 15:44:07 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# projet learn_Tailwind 
---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+import  competencesJson from "../../data/competences.json" assert {type:"json"};
+
+const competences=competencesJson;
+
+console.log(competences);
+const competencesDiv=document.querySelector(".data-competence");
+
+for(let i=0;i<competences.length; i++){
+    // console.log(competences[i].title);
+    const div=document.createElement("div");
+    div.classList.add("box")
+    div.innerHTML=`<div><img src="${competences[i].icon}"></div>"
+    `;
+    competencesDiv.appendChild(div);
+}
